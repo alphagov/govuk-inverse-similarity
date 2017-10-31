e41c51d54644ff41e1f40961722d1f4d683bb258
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-10-31 11:12:38 +0000

    Revert "Nasty multicore hack for performance"
    This commit seems to have added a bug where the results are not passed
    back to the queue.
    
    I'm finding this hard to follow, so to make things work again, revert
    to the pre-multicore state.
    
    This reverts commit 2c28498235f3dbf4adaf38448103c69d236d9bd7.

--- lib/difference_sampler.py
+++ lib/difference_sampler.py
@@ -1,54 +1,22 @@
 import pandas as pd
 from tqdm import tqdm
-from multiprocessing import Process, Queue
-import time
 
 class DifferenceSampler:
   def __init__(self, model, corpus):
     self.model = model
     self.corpus = corpus
 
-  def sample_pages(self, content_dictionary, affinity_threshold, cores=1):
-    content_dictionary['topics'] = self.assign_topics_multicore(cores)
+  def sample_pages(self, content_dictionary, affinity_threshold):
+    content_dictionary['topics'] = self.assign_topics()
     self.topic_affinity_threshold = affinity_threshold
     return self.naive_clustering_by_topic(content_dictionary)
 
-  def assign_topics_multicore(self, cores):
-    pages = self.pages_for_each_core(cores)
-    q = Queue()
-    topics_unsorted = {}
-    sorted_topics = []
-    for i in range(0, cores):
-      process = Process(target=self.assign_topics, args=(q, pages[i], i))
-      process.start()
-    while(True):
-      topics_unsorted.update( q.get() )
-      if len(topics_unsorted) == cores:
-        break
-      time.sleep(10)
-    for i in range(0, cores):
-      sorted_topics.extend(topics_unsorted[i])
-    return sorted_topics
-
-  def pages_for_each_core(self, cores):
-    pages = []
-    pages_per_core = len(self.corpus) / cores
-    for i in range(0, cores):
-      start = pages_per_core * i
-      if i == (cores - 1):
-        end = len(self.corpus)
-      else:
-        end = (pages_per_core * (i+1))
-      pages.append(self.corpus[start:end])
-    return pages
-
-
-  def assign_topics(self, q, corpus_pages, i):
+  def assign_topics(self):
     topics = []
-    print 'Process', i, 'assigning topics to', len(corpus_pages), 'pages'
-    for bow in corpus_pages:
+    print("Assigning topics")
+    for bow in tqdm(self.corpus):
       topics.append(self.model[bow])
-    q.put({i: topics})
+    return topics
 
   def naive_clustering_by_topic(self, content):
     # filter topics with probability < eg 30%
--- sample_spreadsheet_generator.py
+++ sample_spreadsheet_generator.py
@@ -141,7 +141,6 @@ if __name__ == '__main__':
     sampled_pages = DifferenceSampler(model, corpus).sample_pages(
       content_dictionary=content_dictionary,
       affinity_threshold=args.affinity_threshold,
-      cores=args.cores
     )
 
     print str( len(sampled_pages) ) + ' sampled / ' + str( len(content_dictionary) ) + ' total'
